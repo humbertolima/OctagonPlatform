387ab7fd6ac3984e76509ce3422826b2a9b9b151
Humberto Lima <humbertolima870415@outlook.com>
2017-08-16 18:40:55 -0400

    Update

---
+++ OctagonPlatform/Models/EntityConfigurations/CountryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace OctagonPlatform.Models.EntityConfigurations
+{
+    public class CountryConfiguration:EntityTypeConfiguration<Country>
+    {
+
+        public CountryConfiguration()
+        {
+            Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            HasMany(c => c.Stateses)
+                .WithRequired(s => s.Country)
+                .WillCascadeOnDelete(false);
+
+        }
+    }
+}
