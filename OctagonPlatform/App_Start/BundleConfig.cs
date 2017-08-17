efa5d0b1892a0374981e97a9e846659c1eab365e
Humberto Lima <humbertolima870415@outlook.com>
2017-08-17 11:29:15 -0400

    addedbootstrap-cosmo style

--- OctagonPlatform/App_Start/BundleConfig.cs
+++ OctagonPlatform/App_Start/BundleConfig.cs
@@ -29,7 +29,7 @@ namespace OctagonPlatform
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cosmo.css",
                       "~/Content/site.css"));
             
         }
