9a41b492cdb3a050415a91cdb51a8da919213593
Humberto Lima <humbertolima870415@outlook.com>
2017-08-16 22:40:44 -0400

    Nuevas data anotations agregadas en el modelo

--- OctagonPlatform/Helpers/IAuditEntity.cs
+++ OctagonPlatform/Helpers/IAuditEntity.cs
@@ -1,19 +1,29 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace OctagonPlatform.Helpers
 {
     internal interface IAuditEntity
     {
+        [DataType(DataType.DateTime, ErrorMessage = "Created date is not valid")]
+        [Display(Name = "Created At")]
         DateTime? CreatedAt { get; set; }
 
-        int CreatedBy { get; set; }
+        [Display(Name = "Created By")]
+        int? CreatedBy { get; set; }
 
+        [DataType(DataType.DateTime, ErrorMessage = "Created date is not valid")]
+        [Display(Name = "Deleted At")]
         DateTime? DeletedAt { get; set; }
 
-        int DeletedBy { get; set; }
+        [Display(Name = "Deleted By")]
+        int? DeletedBy { get; set; }
 
+        [DataType(DataType.DateTime, ErrorMessage = "Updated date is not valid")]
+        [Display(Name = "Updated At")]
         DateTime? UpdatedAt { get; set; }
 
-        int UpdatedBy { get; set; }
+        [Display(Name = "Updated By")]
+        int? UpdatedBy { get; set; }
     }
 }
--- OctagonPlatform/Models/Partner.cs
+++ OctagonPlatform/Models/Partner.cs
@@ -2,49 +2,77 @@ using OctagonPlatform.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace OctagonPlatform.Models
 {
     public sealed class Partner:IAuditEntity, ISoftDeleted
     {
-
+        [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "The parent partner is required")]
+        [Display(Name = "Parent")]
         public int ParentId { get; set; }
 
         public Partner Parent { get; set; }
 
+        [Required(ErrorMessage = "The partner business's name is required")]
+        [StringLength(50)]
+        [Display(Name = "Business's name")]
         public string BusinessName { get; set; }
 
+        [Required(ErrorMessage = "The partner's status is required")]
         public StatusType.Status Status { get; set; }
 
+        [Required(ErrorMessage = "The address1 is required")]
+        [StringLength(50)]
         public string Address1 { get; set; }
 
+        [StringLength(50)]
         public string Address2 { get; set; }
 
+        [Required(ErrorMessage = "The partner's country is required")]
+        [Display(Name = "Country")]
         public int CountryId { get; set; }
 
         public Country Country { get; set; }
 
+        [Required(ErrorMessage = "The partner's state is required")]
+        [Display(Name = "State")]
         public int StateId { get; set; }
 
         public State State { get; set; }
 
+        [Required(ErrorMessage = "The partner's city is required")]
+        [Display(Name = "City")]
         public int CityId { get; set; }
 
         public City City { get; set; }
 
+        [Required(ErrorMessage = "The email is required")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
+        [Display(Name = "Email Address")]
         public string Email { get; set; }
 
-        public ulong WorkPhone { get; set; }
+        [Display(Name = "Work Phone")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Work phone number is not valid")]
+        public ulong? WorkPhone { get; set; }
 
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(16, ErrorMessage = "Must be between 5 and 50 characters", MinimumLength = 5)]
+        [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Must be a valid email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
         public string Mobile { get; set; }
 
-        public ulong Fax { get; set; }
+        [Display(Name = "Fax Number")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Fax number is not valid")]
+        public ulong? Fax { get; set; }
 
         public string WebSite { get; set; }
 
-        public int LogoId { get; set; }
+        [Display(Name = "Logo")]
+        public int? LogoId { get; set; }
 
         public Logo Logo { get; set; }
 
@@ -68,17 +96,18 @@ namespace OctagonPlatform.Models
             */
         }
 
+        
         public DateTime? CreatedAt { get; set; }
 
-        public int CreatedBy { get; set; }
+        public int? CreatedBy { get; set; }
 
         public DateTime? DeletedAt { get; set; }
-
-        public int DeletedBy { get; set; }
+    
+        public int? DeletedBy { get; set; }
 
         public DateTime? UpdatedAt { get; set; }
 
-        public int UpdatedBy { get; set; }
+        public int? UpdatedBy { get; set; }
 
         public bool? Deleted { get; set; }
     }
