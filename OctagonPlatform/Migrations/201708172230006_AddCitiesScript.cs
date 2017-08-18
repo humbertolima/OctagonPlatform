61192af74895038ab10ab78e9b6c76a5029a61d3
Humberto Lima <humbertolima870415@outlook.com>
2017-08-18 02:37:50 -0400

    Eliminado using System por gusto

--- OctagonPlatform/Migrations/201708172230006_AddCitiesScript.cs
+++ OctagonPlatform/Migrations/201708172230006_AddCitiesScript.cs
@@ -1,8 +1,7 @@
 ﻿namespace OctagonPlatform.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddCitiesScript : DbMigration
     {
         public override void Up()
