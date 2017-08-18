be167012c7b1478b7d07f04afe8e3c28babf5da1
Yasser Osuna <yasser.osuna@hotmail.com>
2017-08-18 15:39:02 -0400

    Adicionado field a tabla user.

--- OctagonPlatform/Models/User.cs
+++ OctagonPlatform/Models/User.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OctagonPlatform.Models
 {
@@ -15,6 +16,20 @@ namespace OctagonPlatform.Models
         [StringLength(16, ErrorMessage = "Must be between 3 and 16 characters", MinimumLength = 3)]
         [Display(Name = "User Name")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "LastName is required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Status { get; set; }
+        
+        public int PartnerId { get; set; }
+        public Partner Partner { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
