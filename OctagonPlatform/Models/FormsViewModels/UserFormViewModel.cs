2a0c81b8c8c9bae77b72e984e7249b5a0a6bd55d
Yasser Osuna <yasser.osuna@hotmail.com>
2017-08-25 11:17:05 -0400

    cambios minimos 1

--- OctagonPlatform/Models/FormsViewModels/UserFormViewModel.cs
+++ OctagonPlatform/Models/FormsViewModels/UserFormViewModel.cs
@@ -42,7 +42,6 @@ namespace OctagonPlatform.Models.FormsViewModels
         public bool IsLocked { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
-        [StringLength(16, ErrorMessage = "Must be between 5 and 50 characters", MinimumLength = 5)]
         [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Must be a valid email")]
         [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
         public string Email { get; set; }
