b52e1aa7af2b7ea66cc97d6b054365cc7cd88f14
Humberto Lima <humbertolima870415@outlook.com>
2017-08-29 17:55:08 -0400

    AddedILogoRepository

---
+++ OctagonPlatform/Models/InterfacesRepository/ILogoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+
+namespace OctagonPlatform.Models.InterfacesRepository
+{
+    public interface ILogoRepository
+    {
+        void UploadLogo(HttpPostedFileBase file);
+
+        void EditLogo(HttpPostedFileBase file, string filePath);
+
+        void DeleteLogo(string filePath);
+    }
+}
---
+++ OctagonPlatform/PersistanceRepository/LogoRepository.cs
@@ -0,0 +1,24 @@
+﻿using OctagonPlatform.Models.InterfacesRepository;
+using System;
+using System.Web;
+
+namespace OctagonPlatform.PersistanceRepository
+{
+    public class LogoRepository: ILogoRepository
+    {
+        public void UploadLogo(HttpPostedFileBase file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditLogo(HttpPostedFileBase file, string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteLogo(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
