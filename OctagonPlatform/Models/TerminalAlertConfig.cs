bbf217cf9755ba31495654fd14d4ab02157d0730
Yasser Osuna <yasser.osuna@hotmail.com>
2017-09-28 12:45:59 -0400

    creado modelos de terminalConfig y terminal message

---
+++ OctagonPlatform/Models/TerminalAlertConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OctagonPlatform.Models
+{
+    public class TerminalAlertConfig
+    {
+        [Key]
+        public int Id { get; set; }
+
+        //pendiente
+        //public Terminal TerminalId { get; set; }      //este sera el enlace con emi.
+        
+        [Required]
+        public double LowCach1 { get; set; }
+
+        public double? LowCash2 { get; set; }
+
+        public double? LowCash3 { get; set; }
+
+        public DateTime? InactivePeriod { get; set; }
+
+        public ICollection<TerminalMessage> MessagesToIgnored { get; set; }
+    }
+}
---
+++ OctagonPlatform/Models/TerminalMessage.cs
@@ -0,0 +1,38 @@
+﻿using OctagonPlatform.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OctagonPlatform.Models
+{
+    public class TerminalMessage : IAuditEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        public string Id_8583 { get; set; }     //enlace con Api de Emi
+
+        public TerminalAlertConfig TerminalAlertConfig { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public int? DeletedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int? UpdatedBy { get; set; }
+        public string UpdatedByName { get; set; }
+        public string CreatedByName { get; set; }
+        public string DeletedByName { get; set; }
+
+        //pendiente poner relacion en Terminals
+        //public ICollection<Terminal> Terminals { get; set; }
+    }
+}
