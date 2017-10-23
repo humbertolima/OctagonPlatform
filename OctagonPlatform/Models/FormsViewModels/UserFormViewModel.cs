8ce4bf08f39bfd60be4a1d73e144c0e5b0db3ba1
Humberto Lima <humbertolima870415@outlook.com>
2017-10-23 14:19:21 -0400

    Rectificar datanotations en user login y resto de modelos de user que no coincidian en password y username

--- OctagonPlatform/Models/FormsViewModels/UserFormViewModel.cs
+++ OctagonPlatform/Models/FormsViewModels/UserFormViewModel.cs
@@ -14,9 +14,9 @@ namespace OctagonPlatform.Models.FormsViewModels
 
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Username is required")]
-        [StringLength(16, ErrorMessage = "Must be between 3 and 16 characters", MinimumLength = 3)]
         [Display(Name = "User Name")]
+        [Required(ErrorMessage = "User name is required")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "Name is required")]
@@ -43,7 +43,8 @@ namespace OctagonPlatform.Models.FormsViewModels
         public IEnumerable<Partner> Partners { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
-        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         //puesto el namespace completo por ambiguedad con el System.Web.MVC
