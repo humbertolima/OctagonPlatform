019ed1e2a8f59a13ce81575063ac9594bb99f105
Yasser Osuna <yasser.osuna@hotmail.com>
2017-11-22 12:16:05 -0500

    agredado codigo en el SEED para agregar partner Oddyssey Group y usuario Admin02

--- OctagonPlatform/Migrations/Configuration.cs
+++ OctagonPlatform/Migrations/Configuration.cs
@@ -13,6 +13,34 @@ namespace OctagonPlatform.Migrations
         {
             //  This method will be called after migrating to the latest version.
 
+            context.Partners.AddOrUpdate(p => p.BusinessName, new Models.Partner
+            {
+                ParentId = 1,
+                BusinessName = "Odyssey Group",
+                Status = Helpers.StatusType.Status.Active,
+                Address1= "753 Shotgum RD",
+                CountryId=1,
+                StateId=1,
+                CityId =1,
+                Email = "admin@xyncro.net",
+                Mobile = "7867921950",
+                Deleted =false,
+            });
+
+            context.Users.AddOrUpdate(u => u.Name, new Models.User
+            {
+                UserName = "admin02",
+                Password = "0C-CD-1F-A5-42-CC-6C-67-CB-7C-3C-B8-CC-86-51-7E",
+                PartnerId = 1,
+                IsLocked = false,
+                Email = "admin@xyncro.net",
+                Name = "Administrator",
+                LastName = "Admin",
+                Phone = "7867921520",
+                Status = Helpers.StatusType.Status.Active,
+                Deleted = false,
+                Key = "t4RcY6PQUBjqO3R24jEYK8d7ZCNS9fuU4QooX1nDSBFJPuKTkNUdiRVv2Uoxu7SPhAw8QDgc7bgiDFsE34JxxqLo54wdO1jVV1Bp",
+            });
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
