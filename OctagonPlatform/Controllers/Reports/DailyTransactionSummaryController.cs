822dbd8792fc2c2871105170668f24cdb47290cb
Emileydis <Emileydis@CASAPC>
2018-01-24 20:37:52 -0500

    dailytransactionsummary add controller

---
+++ OctagonPlatform/Controllers/Reports/DailyTransactionSummaryController.cs
@@ -0,0 +1,116 @@
+﻿
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using Newtonsoft.Json;
+using OctagonPlatform.Controllers.Reports.JSON;
+using OctagonPlatform.Models;
+using OctagonPlatform.Models.FormsViewModels;
+using OctagonPlatform.Models.InterfacesRepository;
+using OctagonPlatform.PersistanceRepository;
+using OctagonPlatform.Views.ReportsSmart.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OctagonPlatform.Controllers.Reports
+{
+    [AllowAnonymous]
+    public class DailyTransactionSummaryController : ReportsSmartController
+    {
+       
+        public DailyTransactionSummaryController(IReports repo, ITerminalRepository repoterminal, IPartnerRepository repopartner, IReportGroup repogroup)
+            :base(repo, repoterminal, repopartner, repogroup)
+        {           
+          
+          
+        }
+
+        public ActionResult DailyTransactionSummary()
+        {
+            DailyTransactionSummaryViewModel model = new DailyTransactionSummaryViewModel();
+            TempData["Chart"] = null;
+            TempData["sub"] = false;
+            return View("DailyTransactionSummary", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DailyTransactionSummary([Bind(Include = "TerminalId,StartDate,EndDate,Partner,PartnerId,Group,GroupId,Surcharge,Dispensed")] DailyTransactionSummaryViewModel vmodel)
+        {
+            ModelState.Remove("PartnerId");
+            ModelState.Remove("GroupId");
+            ModelState.Remove("TerminalId");
+            if (ModelState.IsValid)
+            {
+                List<TransDailyTableVM> listaux = new List<TransDailyTableVM>();
+                List<JsonDailyTransactionSummary> list = new List<JsonDailyTransactionSummary>();
+                ApiATM api = new ApiATM();
+
+                DateTime? start = DateTime.ParseExact(vmodel.StartDate, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+                DateTime? end = DateTime.ParseExact(vmodel.EndDate, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+
+                string[] listtn = ListTerminalByGroup(vmodel.GroupId);
+
+                list = await api.DailyTransactionSummary(start, end, vmodel.TerminalId, listtn, vmodel.Surcharge, vmodel.Dispensed);
+
+                IEnumerable<dynamic> listTn = repo_terminal.TransDailyList(list, vmodel.PartnerId, Convert.ToInt32(Session["partnerId"]));
+
+                if (listTn.Count() > 0)
+                {
+
+
+                    foreach (var item in list)
+                    {
+
+                        string locationname = "";
+
+                        foreach (dynamic x in listTn)
+                        {
+                            if (x.TerminalId == item.TerminalId)
+                            {
+                                locationname = x.LocationName;
+                                break;
+                            }
+                        }
+                        if (locationname != "")
+                        {
+                            TransDailyTableVM obj = new TransDailyTableVM(item.TerminalId, locationname, item.Date, item.ApprovedWithdrawals, item.Declined, item.SurchargableWithdrawals, item.OtherApproved, item.Reversed, item.SurchargeAmount, item.TotalTransaction, item.Surcharge, item.Dispensed);
+
+                            listaux.Add(obj);
+                        }
+                    }
+
+                }
+
+                #region Variables Partial
+
+                TempData["List"] = listaux.Count() > 0 ? listaux : null;
+                TempData["filename"] = "DailyTransactionSummary";
+                TempData["Chart"] = null;
+                TempData["terminal"] = vmodel.TerminalId;
+                TempData["partner"] = vmodel.Partner;
+                TempData["from"] = start?.ToString("MMMM d, yyyy");
+                TempData["to"] = end?.ToString("MMMM d, yyyy");
+                TempData["model"] = vmodel;
+                TempData["sub"] = false;
+                #endregion
+
+                return View("DailyTransactionSummary");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
